a8327b653e83a1f59b66633f32c817b9e37fdfa9
tory1103 <adrian.rubiboy11@gmail.com>
2021-07-30 00:44:39 +0200

    Fixes server host

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from _flaskserver import SERVER, SETTINGS
 if __name__ == '__main__':
     if not len(argv) > 1: argv.append("_")
 
-    SERVER.run(host=SETTINGS["SERVER_ADDR"] if not argv[1] in ["d", "debug"] else SETTINGS["SERVER_ADDR_DEBUG"], port=SETTINGS["SERVER_PORT"] if not argv[1] in ["d", "debug"] else SETTINGS["SERVER_PORT_DEBUG"])
+    SERVER.run(host="0.0.0.0", port=SETTINGS["SERVER_PORT"])
